acda2feacf4b83bc102bf458fac220fcf1ff2fb5
lastpatrol <lastpatrol09@gmail.com>
2021-04-02 17:28:28 +0300

    moved stylish.test.js

--- __tests__/stylish.test.js
+++ __tests__/stylish.test.js
@@ -1,4 +1,4 @@
-import stylish from '../src/stylish.js';
+import stylish from '../src/formatters/stylish.js';
 import { diffStr, diffObj } from '../__fixtures__/expected.js';
 
 test('large', () => {
