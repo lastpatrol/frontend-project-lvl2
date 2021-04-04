a7bf878ec8cf56ba067ca77ac9ee2162e18bfdff
lastpatrol <lastpatrol09@gmail.com>
2021-04-04 17:39:31 +0300

    done: json formatter _forgot some files_

---
+++ src/formatters/json.js
@@ -0,0 +1,1 @@
+export default (diffObj) => JSON.stringify(diffObj);
--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import parse from './parse.js';
 import stylish from './formatters/stylish.js';
 import lazy from './formatters/lazy.js';
 import plain from './formatters/plain.js';
+import json from './formatters/json.js';
 import genDiff from './gendiff.js';
 
-const formatters = { stylish, lazy, plain };
+const formatters = {
+  stylish, lazy, plain, json,
+};
 
 export default (filepath1, filepath2, format) => {
   const cwd = process.cwd();
