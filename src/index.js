3864f2c5a988cc4b996dbcfd55b153aca8c8c527
lastpatrol <lastpatrol09@gmail.com>
2021-04-05 21:54:32 +0300

    fixing hexlet tests...

--- src/index.js
+++ src/index.js
@@ -13,8 +13,5 @@ export default (filepath1, filepath2, format = 'stylish') => {
   const obj1 = parse(filepath1);
   const obj2 = parse(filepath2);
   const diffObj = genDiff(obj1, obj2);
-  console.log(format);
-  console.log(formatters);
-  console.log(diffObj);
   console.log(formatters[format](diffObj));
 };
