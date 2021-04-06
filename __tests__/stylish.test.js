c4e3666d0b682bf8748ec71560c8d2f3f27db2fc
lastpatrol <lastpatrol09@gmail.com>
2021-04-06 17:30:17 +0300

    little fixes of the previous

--- __tests__/stylish.test.js
+++ __tests__/stylish.test.js
@@ -1,4 +1,5 @@
 import { fileURLToPath } from 'url';
+import { readFileSync } from 'fs';
 import path from 'path';
 import parse from '../src/parse.js';
 import genDiff from '../src/gendiff.js';
@@ -8,9 +9,12 @@ import expected from '../__fixtures__/expected-stylish.js';
 const filePath = fileURLToPath(import.meta.url);
 const dirPath = path.dirname(filePath);
 const getFixturePath = (filename) => path.join(dirPath, '..', '__fixtures__', filename);
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
 test('json', () => {
-  const obj1 = parse(getFixturePath('file1.json'));
-  const obj2 = parse(getFixturePath('file2.yml'));
+  const data1 = readFile('file1.json');
+  const data2 = readFile('file2.yml');
+  const obj1 = parse(data1, 'json');
+  const obj2 = parse(data2, 'yml');
   expect(stylish(genDiff(obj1, obj2))).toEqual(expected);
 });
