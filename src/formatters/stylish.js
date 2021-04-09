46e11d16896fc455970475f5a4cca6330aa0184f
lastpatrol <lastpatrol09@gmail.com>
2021-04-09 15:21:37 +0300

    done: stylish - second implementation

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -4,26 +4,54 @@ const stylish = (diffObj) => {
   const block = ' ';
   const indentStep = 4;
   const diffLength = 2;
-  const diffStrings = {
-    1: '+ ',
-    [-1]: '- ',
-    0: '  ',
+  const getDiffStr = (key) => {
+    if (key === '+') return '+ ';
+    if (key === '-') return '- ';
+    return '  ';
   };
 
   const iter = (obj, indentCount) => {
     const indentation = _.repeat(block, indentCount);
-    const arr = obj.flatMap(
-      ({ diff, key, value }) => {
-        const diffStr = diffStrings[diff];
-        if (!Array.isArray(value)) {
-          return `${indentation}${diffStr}${key}: ${value}`;
+    const arrOfStrings = obj.reduce(
+      (acc, node) => {
+        if (node.type === 'toNested') {
+          const value = _.has(node, 'value') ? `${indentation}${getDiffStr('-')}${node.key}: ${node.value}` : [];
+          const nested = `${indentation}${getDiffStr('+')}${node.key}: ${iter(node.children, indentCount + indentStep)}`;
+          return [...acc, value, nested];
         }
 
-        return `${indentation}${diffStr}${key}: ${iter(value, indentCount + indentStep)}`;
+        if (node.type === 'toSimple') {
+          const nested = _.has(node, 'children')
+            ? `${indentation}${getDiffStr('-')}${node.key}: ${iter(node.children, indentCount + indentStep)}` : [];
+          const value = `${indentation}${getDiffStr('+')}${node.key}: ${node.value}`;
+          return [...acc, nested, value];
+        }
+
+        if (node.type === 'removed') {
+          const value = _.has(node, 'value') ? `${indentation}${getDiffStr('-')}${node.key}: ${node.value}` : [];
+          const nested = _.has(node, 'children')
+            ? `${indentation}${getDiffStr('-')}${node.key}: ${iter(node.children, indentCount + indentStep)}` : [];
+          return [...acc, value, nested];
+        }
+
+        if (node.type === 'unchanged') {
+          const value = _.has(node, 'value') ? `${indentation}${getDiffStr()}${node.key}: ${node.value}` : [];
+          const nested = _.has(node, 'children')
+            ? `${indentation}${getDiffStr()}${node.key}: ${iter(node.children, indentCount + indentStep)}` : [];
+          return [...acc, value, nested];
+        }
+
+        if (node.type === 'changedSimple') {
+          const removed = `${indentation}${getDiffStr('-')}${node.key}: ${node.removedValue}`;
+          const added = `${indentation}${getDiffStr('+')}${node.key}: ${node.addedValue}`;
+          return [...acc, removed, added];
+        }
+        throw new Error(`Unknown node type '${node.type}'`);
       },
+      [],
     );
 
-    const joined = arr.join('\n');
+    const joined = arrOfStrings.flat().join('\n');
     const closingBracketIndentation = indentation.slice(0, indentation.length - diffLength);
     return `{\n${joined}\n${closingBracketIndentation}}`;
   };
