128442aa109324c924762704692298b13a3bd7b7
lastpatrol <lastpatrol09@gmail.com>
2021-04-10 18:23:27 +0300

    done: plain v.3

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,22 +1,5 @@
 import _ from 'lodash';
 
-const getDiffType = (obj) => {
-  if ((!_.has(obj, 'children') && obj.type === 'toSimple')
-    || (!_.has(obj, 'value') && obj.type === 'toNested')) {
-    return 'added';
-  }
-  if (obj.type === 'removed') {
-    return 'removed';
-  }
-  if (obj.type === 'changedSimple' || (_.has(obj, 'value') && _.has(obj, 'children'))) {
-    return 'updated';
-  }
-  if (obj.type === 'unchanged' && _.has(obj, 'children')) {
-    return 'unchangedWithChildren';
-  }
-  return 'unchangedWithoutChildren';
-};
-
 const dataToStr = (data) => {
   if (Array.isArray(data)) {
     return '[complex value]';
@@ -28,52 +11,51 @@ const dataToStr = (data) => {
 };
 
 const getRemovedAsStr = (obj) => {
-  if (obj.type === 'removed') {
-    return _.has(obj, 'value') ? dataToStr(obj.value) : dataToStr(obj.children);
+  switch (obj.type) {
+    case 'removed':
+      return _.has(obj, 'value') ? dataToStr(obj.value) : dataToStr(obj.children);
+    case 'toSimple':
+      return _.has(obj, 'children') ? dataToStr(obj.children) : dataToStr(obj.removedValue);
+    case 'toNested':
+      return dataToStr(obj.value);
+    default:
+      throw new Error(`Cannot find removed in:\n${JSON.stringify(obj)}`);
   }
-  if (obj.type === 'toNested' && _.has(obj, 'value')) {
-    return dataToStr(obj.value);
-  }
-  if (obj.type === 'toSimple' && _.has(obj, 'children')) {
-    return dataToStr(obj.children);
-  }
-  if (_.has(obj, 'removedValue')) {
-    return dataToStr(obj.removedValue);
-  }
-  throw new Error(`Cannot find removed in:\n${JSON.stringify(obj)}`);
 };
 
 const getAddedAsStr = (obj) => {
-  if (obj.type === 'toNested') {
-    return dataToStr(obj.children);
+  switch (obj.type) {
+    case 'added':
+      return _.has(obj, 'value') ? dataToStr(obj.value) : dataToStr(obj.children);
+    case 'toSimple':
+      return _.has(obj, 'value') ? dataToStr(obj.value) : dataToStr(obj.addedValue);
+    case 'toNested':
+      return dataToStr(obj.children);
+    default:
+      throw new Error(`Cannot find added in:\n${JSON.stringify(obj)}`);
   }
-  if (obj.type === 'toSimple') {
-    return dataToStr(obj.value);
-  }
-  if (_.has(obj, 'addedValue')) {
-    return dataToStr(obj.addedValue);
-  }
-  throw new Error(`Cannot find added in:\n${JSON.stringify(obj)}`);
 };
 
 const plain = (diffObj) => {
   const iter = (entries, path) => entries.reduce(
     (acc, obj) => {
-      const { key } = obj;
+      const { key, type } = obj;
       const fullPath = path.length === 0 ? `${key}` : `${path}.${key}`;
-      const diffType = getDiffType(obj);
 
-      switch (diffType) {
+      switch (type) {
         case 'added':
           return [...acc, `Property '${fullPath}' was added with value: ${getAddedAsStr(obj)}`];
         case 'removed':
           return [...acc, `Property '${fullPath}' was removed`];
-        case 'updated':
+        case 'toSimple':
+        case 'toNested':
           return [...acc, `Property '${fullPath}' was updated. From ${getRemovedAsStr(obj)} to ${getAddedAsStr(obj)}`];
-        case 'unchangedWithChildren':
-          return [...acc, ...iter(obj.children, fullPath)];
+        case 'unchanged':
+          return (_.has(obj, 'children'))
+            ? [...acc, ...iter(obj.children, fullPath)]
+            : acc;
         default:
-          return acc;
+          throw new Error(`Unknown type! Type: '${type}', key '${key}'`);
       }
     },
     [],
