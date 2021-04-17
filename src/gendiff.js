0c4992910541a23158a2360f1d10d13b2b586d31
lastpatrol <lastpatrol09@gmail.com>
2021-04-17 22:53:00 +0300

    done: gendiff v.4

--- src/gendiff.js
+++ src/gendiff.js
@@ -5,77 +5,28 @@ const buildDiff = (obj1, obj2) => {
 
   return unitedKeys.map(
     (key) => {
-      if (!_.has(obj1, key) && _.isPlainObject(obj2[key])) {
-        return {
-          key,
-          type: 'added',
-          children: buildDiff(obj2[key], obj2[key]),
-        };
-      }
-
-      if (!_.has(obj1, key) && !_.isPlainObject(obj2[key])) {
+      if (!_.has(obj1, key) && _.has(obj2, key)) {
         return {
           key,
           type: 'added',
           value: obj2[key],
         };
       }
-
-      if (!_.has(obj2, key) && _.isPlainObject(obj1[key])) {
-        return {
-          key,
-          type: 'removed',
-          children: buildDiff(obj1[key], obj1[key]),
-        };
-      }
-
-      if (!_.has(obj2, key) && !_.isPlainObject(obj1[key])) {
+      if (_.has(obj1, key) && !_.has(obj2, key)) {
         return {
           key,
           type: 'removed',
           value: obj1[key],
         };
       }
-
-      const value1 = obj1[key];
-      const value2 = obj2[key];
-
-      if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
+      if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
         return {
           key,
-          type: 'unchanged',
+          type: 'nested',
           children: buildDiff(obj1[key], obj2[key]),
         };
       }
-
-      if (_.isPlainObject(value1)) {
-        return {
-          key,
-          type: 'toSimple',
-          value: obj2[key],
-          children: buildDiff(obj1[key], obj1[key]),
-        };
-      }
-
-      if (_.isPlainObject(value2)) {
-        return {
-          key,
-          type: 'toNested',
-          value: obj1[key],
-          children: buildDiff(obj2[key], obj2[key]),
-        };
-      }
-
-      if (value1 !== value2) {
-        return {
-          key,
-          type: 'toSimple',
-          removedValue: obj1[key],
-          addedValue: obj2[key],
-        };
-      }
-
-      if (value1 === value2) {
+      if (obj1[key] === obj2[key]) {
         return {
           key,
           type: 'unchanged',
@@ -83,7 +34,12 @@ const buildDiff = (obj1, obj2) => {
         };
       }
 
-      throw new Error('Something went wrong in genDiff...');
+      return {
+        key,
+        type: 'changed',
+        oldValue: obj1[key],
+        newValue: obj2[key],
+      };
     },
   );
 };
