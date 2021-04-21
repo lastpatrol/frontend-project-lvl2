16797f5d8154c153ccdfcd849a286ee7d2e5d7f8
lastpatrol <lastpatrol09@gmail.com>
2021-04-21 20:42:10 +0300

    reformatted code in gendiff.js

--- src/gendiff.js
+++ src/gendiff.js
@@ -2,43 +2,22 @@ import _ from 'lodash';
 
 const buildDiff = (obj1, obj2) => {
   const unitedKeys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
-
   return unitedKeys.map(
     (key) => {
       if (!_.has(obj1, key) && _.has(obj2, key)) {
-        return {
-          key,
-          type: 'added',
-          value: obj2[key],
-        };
+        return { key, type: 'added', value: obj2[key] };
       }
       if (_.has(obj1, key) && !_.has(obj2, key)) {
-        return {
-          key,
-          type: 'removed',
-          value: obj1[key],
-        };
+        return { key, type: 'removed', value: obj1[key] };
       }
       if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
-        return {
-          key,
-          type: 'nested',
-          children: buildDiff(obj1[key], obj2[key]),
-        };
+        return { key, type: 'nested', children: buildDiff(obj1[key], obj2[key]) };
       }
       if (obj1[key] === obj2[key]) {
-        return {
-          key,
-          type: 'unchanged',
-          value: obj1[key],
-        };
+        return { key, type: 'unchanged', value: obj1[key] };
       }
-
       return {
-        key,
-        type: 'changed',
-        oldValue: obj1[key],
-        newValue: obj2[key],
+        key, type: 'changed', oldValue: obj1[key], newValue: obj2[key]
       };
     },
   );
